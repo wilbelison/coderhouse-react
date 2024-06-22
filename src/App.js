b4b0fdbfb78df97562eb87541bf63b9f79f44aa0
Wilbelison Junior <meustudio@gmail.com>
2024-06-22 09:13:08 -0300

    Component NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/fonts.css';
+import './assets/styles/index.css';
+
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,31 @@
+import logo from "../../assets/images/logo.svg";
+
+const links = [
+  { value: "Ação e Aventura", href: "#acao-aventura" },
+  { value: "Luta e Esportes", href: "#luta-esportes" },
+  { value: "Plataforma", href: "#plataforma" },
+  { value: "Puzzle e Casual", href: "#puzzle-casual" },
+  { value: "RPG e Estratégia", href: "#rpg-estrategia" },
+  { value: "Mais Gêneros", href: "#mais-generos" },
+];
+
+const menu = links.map((item) => (
+  <li>
+    <a href={item.href} title={item.value}>
+      {item.value}
+    </a>
+  </li>
+));
+
+function NavBar() {
+  return (
+    <nav className="NavBar">
+      <a href="/" className="logo" title="Ir para a home da Gameflix Store">
+        <img src={logo} alt="Gameflix" /> Store
+      </a>
+      <ul className="menu">{menu}</ul>
+    </nav>
+  );
+}
+
+export default NavBar;
